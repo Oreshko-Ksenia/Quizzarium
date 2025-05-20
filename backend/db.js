71762479519f5210812675b0a39b278aad205152
Oreshko-Ksenia <119946576+Oreshko-Ksenia@users.noreply.github.com>
2025-05-20 20:56:58 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,19 @@
-const {Sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 module.exports = new Sequelize(
-    process.env.PG_DB || 'QuizDB',
-    process.env.PG_USER || 'postgres', 
-    process.env.PG_PASSWORD || '12345',
-    {
-        host: process.env.PG_HOST || 'localhost',
-        port: process.env.PG_PORT || '5432',
-        dialect: 'postgres',
-    }
-);
+  process.env.PG_DB || 'QuizDB',
+  process.env.PG_USER || 'postgres',
+  process.env.PG_PASSWORD || '12345',
+  {
+    host: process.env.PG_HOST || 'localhost',
+    port: process.env.PG_PORT || 5432,
+    dialect: 'postgres',
 
+    dialectOptions: {
+      ssl: {
+        require: true, 
+        rejectUnauthorized: false 
+      }
+    }
+  }
+);
